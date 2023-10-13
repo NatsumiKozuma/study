ef53f3e5749cd424f3cefebc7c68a317616ee540
NatsumiKozuma <17ui044@gmail.com>
2023-10-13 12:14:36 +0900

    課題提出

---
+++ JavaStudy/Springt0006/src/main/java/com/example/demo/Springt0006Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springt0006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springt0006Application.class, args);
+	}
+
+}
---
+++ JavaStudy/Springt0006/src/test/java/com/example/demo/Springt0006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springt0006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
